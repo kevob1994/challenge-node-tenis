a7ed91c3b3804b598f81c13dfa13372c97c32207
kevob1994 <kevob1994@gmail.com>
2023-02-14 00:16:30 -0300

    Fix config for migration

--- apps/server/src/db.ts
+++ apps/server/src/db.ts
@@ -3,11 +3,12 @@ import { User } from './models';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'process.env.DATABASE_HOST',
+  host: process.env.DATABASE_HOST || 'localhost',
   port: Number(process.env.DATABASE_PORT) || 5432,
-  username: process.env.DATABASE_USER || 'process.env.DATABASE_USER',
-  password: process.env.DATABASE_PASSWORD || 'process.env.DATABASE_PASSWORD',
-  database: process.env.DATABASE_NAME || 'process.env.DATABASE_NAME',
-  entities: [User],
+  username: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'admin',
+  database: process.env.DATABASE_NAME || 'nodus-challenge',
   synchronize: true,
+  entities: [User],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
 });
